026010dbaef2950da9b429fea959598a488ccfc2
NickeaBennett <nickeabennett@gmail.com>
2022-07-26 10:12:23 -0400

    commit tree c4199434798ecea0540f40d8d27ff495daa42598

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,25 @@
-# Write your code here.
-require 'pry'
-# katz_deli = []
-
-def line(katz_deli)
-    if katz_deli.empty?
-            puts "The line is currently empty."
-        else
-            in_line = "The line is currently: "   
-            katz_deli.map.with_index(1) do |customer, i| 
-            in_line << "#{i}. #{customer}"
-        end
-        puts in_line
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
     end
-end
-
-def take_a_number(katz_deli, customer_name)
-    katz_deli << customer_name
-    puts "Welcome, #{customer_name}. You are number #{katz_deli.length} in line."
-end
-
-def now_serving(katz_deli)
-    if katz_deli.empty?
-      puts "There is nobody waiting to be served!"
-    else
-      puts "Currently serving #{katz_deli.first}."
-      deli.shift
-    end
+    puts current_line
   end
+end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
+end
